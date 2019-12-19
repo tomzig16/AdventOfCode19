d30f795352d13134668e7c9169f64e95275d2823
tomzig16 <tzigmantavicius@gmail.com>
2019-12-19 15:09:15 +0200

    Solve Day4 (2)

--- src/Day4/Program.cs
+++ src/Day4/Program.cs
@@ -20,6 +20,14 @@ namespace Day4
                 if (IsAccordingToRules(i)) nOfPossiblePasswords++;
             }
             Console.WriteLine($"Result: {nOfPossiblePasswords}");
+
+            Console.WriteLine("Part 2");
+            nOfPossiblePasswords = 0;
+            for (int i = inputLowest; i < inputHighest; i++)
+            {
+                if (IsAccordingToRulesPart2(i)) nOfPossiblePasswords++;
+            }
+            Console.WriteLine($"Result: {nOfPossiblePasswords}");
             Console.ReadKey();
         }
 
@@ -40,6 +48,48 @@ namespace Day4
                     (digits[i] == digits[i - 1]))
                 {
                     hasPair = true;
+                }
+            }
+            return hasPair;
+        }
+
+        static bool IsAccordingToRulesPart2(int number)
+        {
+            int[] digits = ToDigitArray(number);
+
+            bool hasPair = false;
+
+            for(int i = 1; i < digits.Length; i++)
+            {
+                if (digits[i - 1] > digits[i])
+                {
+                    // Number decreased
+                    return false;
+                }
+            }
+
+            for (int i = 1; i < digits.Length; i++)
+            {
+                if (digits[i] == digits[i - 1])
+                {
+                    if (i != digits.Length - 1)
+                    {
+                        int nOfSameNumbers = 1; // 1 for i - 1
+                        for (int j = i; digits[j] == digits[i]; j++)
+                        {
+                            nOfSameNumbers++;
+                            if (j == digits.Length - 1) break;
+                        }
+                        if(nOfSameNumbers == 2)
+                        {
+                            hasPair = true;
+                        }
+                        i += nOfSameNumbers - 2;
+                    }
+                    else
+                    {
+                        hasPair = true;
+                    }
                 }
             }
             return hasPair;
